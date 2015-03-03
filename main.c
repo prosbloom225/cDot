3a9533bea03a2fa1c2c634d22f75af32e13abf55
prosbloom225 <prosbloom@gmail.com>
2015-03-03 15:37:13 -0600

    fixing relative links for path_to_dotfiles

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -18,15 +19,25 @@ int main(int argc, char** argv) {
 	char buff[128];
 	memset(buff,0,sizeof(buff));
 	strcpy(buff, argv[1]);
+	// Relative pathing
+	char path_to_dotfiles[256];	
+	realpath(argv[1], path_to_dotfiles);
+#ifndef DEBUG_OUT
+	printf("Realpath: %s\n", path_to_dotfiles);
+#endif
 	// Check that it exists, if not return 1
-	if (NULL == (dp = opendir(argv[1])) ) {
-		printf("Cannot open input directory [%s]\n", argv[1]);
+	if (NULL == (dp = opendir(path_to_dotfiles)) ) {
+		printf("Cannot open input directory [%s]\n", path_to_dotfiles);
 		return 1;
 	} else {
-		// Set the DOTFOLDER to ~ if undefined
+		// Set the DOTFOLDER to argv[2], else ~ if undefined
 #ifndef DOTFOLDER
-		struct passwd *pw = getpwuid(getuid());
-		const char* DOTFOLDER= pw->pw_dir;
+		if (argc == 2) {
+			const char* DOTFOLDER = argv[2];
+		} else {
+			struct passwd *pw = getpwuid(getuid());
+			const char* DOTFOLDER= pw->pw_dir;
+		}
 #endif
 #ifdef DEBUG_OUT
 		printf("Dotfolder: %s\n", DOTFOLDER);
@@ -39,18 +50,20 @@ int main(int argc, char** argv) {
 				// Build paths
 				char initial_file_location[256];
 				char link_file_location[256];
-				snprintf(initial_file_location, sizeof initial_file_location, "%s/%s", argv[1], dir->d_name);
+				snprintf(initial_file_location, sizeof initial_file_location, "%s/%s", path_to_dotfiles, dir->d_name);
 				snprintf(link_file_location, sizeof link_file_location, "%s/%s", DOTFOLDER, dir->d_name);
 #ifdef DEBUG_OUT
 				printf("%s -> %s\n", initial_file_location, link_file_location);
 #endif
 				// Symlink
 				int ret = symlink(initial_file_location, link_file_location);
-				if (ret != 0) {
+#ifdef DEBUG_OUT
+				if (ret != 0) 
 					printf("ERROR Symlinking: %i", ret);
-				}
+#endif
 			}
 		}
+		printf("Symlinking complete!\n");
 		closedir(dp);
 	}
 	return 0;
